5f44ccdfbb74bf0663fdfa54c6390d87d834750e
abow <mike79.tseng@gmail.com>
2022-03-16 23:17:46 +0800

    Laravel軟體刪除(softdelete)

--- app/Models/animal.php
+++ app/Models/animal.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class animal extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     /*
     可以被批量寫入的屬性(欄位)
     @param  array  $attributes
